ba304675f1457bad662e362d427a2b6225a9d4f8
Sudharshan Madhavan <msudharshan06@gmail.com>
2024-08-24 17:42:37 -0700

    CSS changes and Footer aligned

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styles: `.social-links {
+    align-items: center;
+    gap: 0.73rem;
+    position: relative;
+    text-align: right;
+  }
+
+  .social-links path {
+    transition: fill 0.3s ease;
+    fill: var(--gray-400);
+  }
+
+  .social-links a:hover svg path {
+    fill: var(--gray-900);
+  }`
+})
+export class FooterComponent {
+
+}
